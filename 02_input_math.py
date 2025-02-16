fbda8639d06472e42f7cd7f129b0e99cff63b072
oocurry <aameyashrivastav@rhs.school.nz>
2025-02-17 10:17:23 +1300

    Create 02_input_math.py

---
+++ 02_input_math.py
@@ -0,0 +1,40 @@
+#Comment your code (ie: # this is a comment)
+#User input can be stored in variables and then manipulated
+#We can use the 'dot format' method to control exactly how our output appears.
+#If you enter text when a number is expected, the program will crash 
+#(unless you implement code to handle the error correctly)
+
+# get input
+
+# ask user for name
+name = input("What is your name? ")
+
+# ask user for two numbers
+
+# add numbers together
+
+# multiply numbers together
+
+# greet user and display math
+print('Hello')
+print("Hello", name)
+
+# get input
+
+# ask user for name
+name = input("What is your name? ")
+
+# ask user for two numbers
+num_1 = int(input("What is your favourite number? "))
+num_2 = int(input("What is your second favourite number?"))
+
+# add numbers together
+add = num_1 + num_2
+
+# multiply numbers together
+
+# greet user and display math
+print("Hello", name)
+
+print("{} + {} = {}".format(num_1, num_2, add))
+
